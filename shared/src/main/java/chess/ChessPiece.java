1aadbba2e2a0d82f2c57b494414949fe7423bb84
Jay Lowry <jay.lowry350@gmail.com>
2024-07-02 10:30:11 -0600

    Rook Move tests passed

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -68,6 +68,7 @@ public class ChessPiece {
             case KNIGHT:
                 break;
             case ROOK:
+                moveList = rookMoves(board, myPosition, row, col);
                 break;
             case PAWN:
                 break;
@@ -106,5 +107,109 @@ public class ChessPiece {
             }
         }
         return kingMoveList;
+    }
+
+    private Collection<ChessMove> rookMoves(ChessBoard board, ChessPosition myPosition, int row, int col) {
+        Collection<ChessMove> rookMoveList = new ArrayList<>();
+        boolean isRightBlocked = false;
+        boolean isLeftBlocked = false;
+        boolean isUpBlocked = false;
+        boolean isDownBlocked = false;
+
+        //Vertical and Horizontal
+        for (int i = 1; i <= 8; i++) {
+            //Right
+            if (!isRightBlocked) {
+                ChessPosition rightGuess = new ChessPosition(row, col+i);
+
+                // First check for range errors
+                if (col+i > 8) {
+                    isRightBlocked = true;
+                }
+
+                // Then check to see if there is a piece already on the square
+                if (board.getPiece(rightGuess) != null && !isRightBlocked) {
+                    if (board.getPiece(rightGuess).getTeamColor() != this.getTeamColor()) {
+                        rookMoveList.add(new ChessMove(myPosition, rightGuess, null));
+                    }
+                    isRightBlocked = true;
+                }
+
+                // If none of the above, is valid move
+                if (!isRightBlocked) {
+                    rookMoveList.add(new ChessMove(myPosition, rightGuess, null));
+                }
+            }
+
+            //Left
+            if (!isLeftBlocked) {
+                ChessPosition leftGuess = new ChessPosition(row, col-i);
+
+                // First check for range errors
+                if (col-i < 1) {
+                    isLeftBlocked = true;
+                }
+
+                // Then check to see if there is a piece already on the square
+                if (board.getPiece(leftGuess) != null && !isLeftBlocked) {
+                    if (board.getPiece(leftGuess).getTeamColor() != this.getTeamColor()) {
+                        rookMoveList.add(new ChessMove(myPosition, leftGuess, null));
+                    }
+                    isLeftBlocked = true;
+                }
+
+                // If none of the above, is valid move
+                if (!isLeftBlocked) {
+                    rookMoveList.add(new ChessMove(myPosition, leftGuess, null));
+                }
+            }
+
+            //Up
+            if (!isUpBlocked) {
+                ChessPosition upGuess = new ChessPosition(row+i, col);
+
+                // First check for range errors
+                if (row+i > 8) {
+                    isUpBlocked = true;
+                }
+
+                // Then check to see if there is a piece already on the square
+                if (board.getPiece(upGuess) != null && !isUpBlocked) {
+                    if (board.getPiece(upGuess).getTeamColor() != this.getTeamColor()) {
+                        rookMoveList.add(new ChessMove(myPosition, upGuess, null));
+                    }
+                    isUpBlocked = true;
+                }
+
+                // If none of the above, is valid move
+                if (!isUpBlocked) {
+                    rookMoveList.add(new ChessMove(myPosition, upGuess, null));
+                }
+            }
+
+            //Down
+            if (!isDownBlocked) {
+                ChessPosition downGuess = new ChessPosition(row-1, col);
+
+                // First check for range errors
+                if (row-i < 1) {
+                    isDownBlocked = true;
+                }
+
+                // Then check to see if there is a piece already on the square
+                if (board.getPiece(downGuess) != null && !isDownBlocked) {
+                    if (board.getPiece(downGuess).getTeamColor() != this.getTeamColor()) {
+                        rookMoveList.add(new ChessMove(myPosition, downGuess, null));
+                    }
+                    isDownBlocked = true;
+                }
+
+                // If none of the above, is valid move
+                if (!isDownBlocked) {
+                    rookMoveList.add(new ChessMove(myPosition, downGuess, null));
+                }
+            }
+        }
+        return rookMoveList;
     }
 }
