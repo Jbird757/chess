aa150c416c1b8815da305a90da2823e72ace49db
Jbird757 <78626717+Jbird757@users.noreply.github.com>
2024-07-23 16:11:35 -0600

    added unit test classes

--- server/src/test/java/service/ClearServiceTest.java
+++ server/src/test/java/service/ClearServiceTest.java
@@ -3,12 +3,15 @@ package service;
 import model.UserData;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class ClearServiceTest {
 
     @Test
-    void clearAllDB() {
+    void clearEmptyDB() {
+
+    }
+
+    @Test
+    void clearPopulatedDB() {
         UserData newUser = new UserData("newUser1", "byu1234", "newemail@email.com");
 
     }
---
+++ server/src/test/java/service/UserServiceTest.java
@@ -0,0 +1,14 @@
+package service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    void createNewNormalUser() {
+        
+    }
+
+}
