1bf4bc5c64db6c2f2fb2a98f2a2797c15df0c367
Jbird757 <78626717+Jbird757@users.noreply.github.com>
2024-08-03 16:36:11 -0600

    created structure, started ui

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,55 @@
 import chess.*;
 
+import java.util.Scanner;
+
 public class Main {
+    static String username = "LOGGED_OUT";
+
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        System.out.println("Welcome to online Chess. Choose one of the options below to get started.");
+        printPrelogin();
+        preLoginConsole();
+    }
+
+    private static void preLoginConsole() {
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+
+        while (!quit) {
+            System.out.print("["+username+"] >> ");
+            String input = scanner.nextLine();
+            String[] words = input.split(" ");
+
+            switch (words[0].toLowerCase()) {
+                case "register":
+                    break;
+                case "login":
+                    break;
+                case "quit":
+                    quit = true;
+                case "help":
+                    printPrelogin();
+            }
+        }
+
+        scanner.close();
+    }
+
+    private static void printPrelogin() {
+        System.out.println("\nCOMMANDS:");
+        System.out.println("register <USERNAME> <PASSWORD> <EMAIL>\t\tCreates a new account");
+        System.out.println("login <USERNAME> <PASSWORD> \t\t\t\tLogs in an existing user");
+        System.out.println("quit \t\t\t\t\t\t\t\t\t\tExit Application");
+        System.out.println("help \t\t\t\t\t\t\t\t\t\tView these commands at any time");
+    }
+
+    private static void printPostlogin() {
+        System.out.println("\nCOMMANDS:");
+        System.out.println("create <GAMENAME> \t\tCreates a new game with the specified name");
+        System.out.println("list \t\t\t\t\tDisplays a list of active games");
+        System.out.println("join <ID> \t\t\t\tJoins a game with the specified ID");
+        System.out.println("observe <ID> \t\t\tWatch a game with the specified ID");
+        System.out.println("logout \t\t\t\t\tLogs out the current user");
+        System.out.println("help \t\t\t\t\tView these commands at any time");
     }
 }
