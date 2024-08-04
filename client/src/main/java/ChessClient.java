dfb403ca7f1bf286c7312b29afe1f1a0b4867a77
Jbird757 <78626717+Jbird757@users.noreply.github.com>
2024-08-03 18:22:49 -0600

    Added helper functions to handle exceptions

--- client/src/main/java/ChessClient.java
+++ client/src/main/java/ChessClient.java
@@ -26,18 +26,19 @@ public class ChessClient {
             switch (words[0].toLowerCase()) {
                 case "register":
                     if (words.length != 4) {
-                        System.out.println("Error: missing arguments");
+                        System.out.println("Error: invalid arguments");
                         break;
                     }
                     System.out.print("Registering user...");
-                    server.registerUser(words[1], words[2], words[3]);
+                    registerUser(words[1], words[2], words[3]);
                     break;
                 case "login":
                     if (words.length != 3) {
-                        System.out.println("Error: missing arguments");
+                        System.out.println("Error: invalid arguments");
                         break;
                     }
                     System.out.print("Logging in...");
+                    login(words[1], words[2]);
                     postLoginConsole();
                     break;
                 case "quit":
@@ -66,15 +67,29 @@ public class ChessClient {
 
             switch (words[0].toLowerCase()) {
                 case "create":
+                    if (words.length != 2) {
+                        System.out.println("Error: invalid arguments");
+                    }
+                    createGame(words[1]);
                     break;
                 case "list":
+                    listGames();
                     break;
                 case "join":
+                    if (words.length != 3) {
+                        System.out.println("Error: invalid arguments");
+                    }
+                    joinGame(Integer.parseInt(words[1]), words[2]);
                     break;
                 case "observe":
+                    if (words.length != 2) {
+                        System.out.println("Error: invalid arguments");
+                    }
+                    observeGame(Integer.parseInt(words[1]));
                     break;
                 case "logout":
                     this.username = "LOGGED_OUT";
+                    logout();
                     quit = true;
                     break;
                 case "help":
@@ -85,6 +100,69 @@ public class ChessClient {
             }
         }
         scanner.close();
+    }
+
+    private void registerUser(String username, String password, String email) {
+        try {
+            server.registerUser(username, password, email);
+        } catch (Exception e) {
+            var msg = e.getMessage();
+            System.out.println(msg);
+        }
+    }
+
+    private void login(String username, String password) {
+        try {
+            server.login(username, password);
+        } catch (Exception e) {
+            var msg = e.getMessage();
+            System.out.println(msg);
+        }
+    }
+
+    private void createGame(String gameName) {
+        try {
+            server.createGame(gameName);
+        } catch (Exception e) {
+            var msg = e.getMessage();
+            System.out.println(msg);
+        }
+    }
+
+    private void listGames() {
+        try {
+            server.listGames();
+        } catch (Exception e) {
+            var msg = e.getMessage();
+            System.out.println(msg);
+        }
+    }
+
+    private void joinGame(int gameID, String playerColor) {
+        try {
+            server.joinGame(gameID, playerColor);
+        } catch (Exception e) {
+            var msg = e.getMessage();
+            System.out.println(msg);
+        }
+    }
+
+    private void observeGame(int gameID) {
+        try {
+            server.observeGame(gameID);
+        } catch (Exception e) {
+            var msg = e.getMessage();
+            System.out.println(msg);
+        }
+    }
+
+    private void logout() {
+        try {
+            server.logout();
+        } catch (Exception e) {
+            var msg = e.getMessage();
+            System.out.println(msg);
+        }
     }
 
     private void printPreLoginHelp() {
@@ -99,7 +177,7 @@ public class ChessClient {
         System.out.println("\nCOMMANDS:");
         System.out.println("create <GAMENAME> \t\tCreates a new game with the specified name");
         System.out.println("list \t\t\t\t\tDisplays a list of active games");
-        System.out.println("join <ID> \t\t\t\tJoins a game with the specified ID");
+        System.out.println("join <ID> [WHITE|BLACK] \t\t\tJoins a game with the specified ID");
         System.out.println("observe <ID> \t\t\tWatch a game with the specified ID");
         System.out.println("logout \t\t\t\t\tLogs out the current user");
         System.out.println("help \t\t\t\t\tView these commands at any time");
--- client/src/main/java/ServerFacade.java
+++ client/src/main/java/ServerFacade.java
@@ -1,6 +1,8 @@
+import dataaccess.DataAccessException;
 import model.AuthData;
 import model.GameData;
 import model.UserData;
+import com.google.gson.Gson;
 
 import java.io.*;
 import java.net.*;
@@ -12,48 +14,94 @@ public class ServerFacade {
         this.serverURL = serverURL;
     }
 
-    public AuthData registerUser(String username, String Password, String email) {
+    public AuthData registerUser(String username, String Password, String email) throws DataAccessException {
         UserData user = new UserData(username, Password, email);
         var path = "/user";
-        return null;
+        return this.makeRequest("POST", path, user, AuthData.class);
     }
 
-    public AuthData login(String username, String Password) {
+    public AuthData login(String username, String Password) throws DataAccessException {
         var path = "/session";
+        System.out.println("You are logged in");
         return null;
     }
 
-    public void logout(String authToken) {
+    public void logout() throws DataAccessException {
         var path = "/session";
+        System.out.println("You are logged out");
     }
 
-    public GameData[] listGames() {
+    public GameData[] listGames() throws DataAccessException {
+        var path = "/game";
+        System.out.println("List of games");
+        return null;
+    }
+
+    public GameData createGame(String gameName) throws DataAccessException {
+        var path = "/game";
+        System.out.println("Creating game " + gameName);
+        return null;
+    }
+
+    public void joinGame(int gameID, String playerColor) throws DataAccessException {
+        System.out.println("Joining game " + gameID);
+        var path = "/game";
+    }
+
+    public GameData observeGame(int gameID) throws DataAccessException {
         var path = "/game";
         return null;
     }
 
-    public GameData createGame(String gameName) {
-        var path = "/game";
-        return null;
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws DataAccessException {
+        try {
+            URL url = (new URI(serverURL + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+            writeBody(request, http);
+            http.connect();
+            throwIfNotSuccessful(http);
+            return readBody(http, responseClass);
+        } catch (Exception ex) {
+            throw new DataAccessException(ex.getMessage());
+        }
     }
 
-    public void joinGame(int gameID, String username, String color) {
-        var path = "/game";
+
+    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if (request != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
     }
 
-//    private <T> T makeRequest(String method, String path, Object request, Class<T> responseType) {
-//        try {
-//            URL url = (new URI(serverURL + path)).toURL();
-//            HttpURLConnection http = (HttpURLConnection) url.openConnection();
-//            http.setRequestMethod(method);
-//            http.setDoOutput(true);
-//
-//            writeBody(request, http);
-//            http.connect();
-//            throwIfNotSuccessful(http);
-//            return readBody(http, responseClass);
-//        } catch (Exception ex) {
-//            throw new ResponseException(500, ex.getMessage());
-//        }
-//    }
+    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, DataAccessException {
+        var status = http.getResponseCode();
+        if (!isSuccessful(status)) {
+            throw new DataAccessException("failure: " + status);
+        }
+    }
+
+    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                if (responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
+    }
+
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
 }
